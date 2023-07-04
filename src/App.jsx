78e451b7b9ceceab4d3687931cccde39a673bc13
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-04 09:24:42 -0400

    Include base jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,22 @@ function App() {
 
 
   return (
-    <>
-    </>
+    <div className='app'>
+      <h1>Seção de Comentários</h1>
+      <div className='inputs'>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" />
+      </div>
+      <div className='inputs'>
+        <label htmlFor="comment">Comentário:</label>
+        <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+      </div>
+      <button>Enviar comentário</button>
+      <hr />
+      <div>
+        <p>Seja o primeiro a comentar!</p>
+      </div>
+    </div>
   )
 }
 
